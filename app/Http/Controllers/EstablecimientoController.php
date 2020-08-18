b7ad47e4910edbe57aeec5d00a492ab44dffb293
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-08-18 18:40:23 -0400

    insertamos valores a la base de datos con seeder

---
+++ app/Http/Controllers/EstablecimientoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\establecimiento;
+use Illuminate\Http\Request;
+
+class EstablecimientoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\establecimiento  $establecimiento
+     * @return \Illuminate\Http\Response
+     */
+    public function show(establecimiento $establecimiento)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\establecimiento  $establecimiento
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(establecimiento $establecimiento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\establecimiento  $establecimiento
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, establecimiento $establecimiento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\establecimiento  $establecimiento
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(establecimiento $establecimiento)
+    {
+        //
+    }
+}
