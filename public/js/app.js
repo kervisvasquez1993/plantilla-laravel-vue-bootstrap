5b8622b0ef3be6bdd5acfb76ff8b0a7c624b517e
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-03-19 22:14:15 -0400

    agregar nuevos campos para establecimiento

--- public/js/app.js
+++ public/js/app.js
@@ -64081,8 +64081,8 @@ document.addEventListener('DOMContentLoaded', function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/html/bodegas/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /var/www/html/bodegas/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/html/plantilla-laravel-vue-bootstrap/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/plantilla-laravel-vue-bootstrap/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
